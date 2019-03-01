f63923826fc06f2b65f1a870b5afb72e0b69cceb
Dan Rapp <drapp@proofpoint.com>
2019-03-01 15:45:09 -0700

    Add sample extract.json file to repository Handel specifying region in extract.json to allow setting of jupyter password or token via credstash (functionality is in base docker-ds image)

--- Dockerfile
+++ Dockerfile
@@ -7,5 +7,8 @@ ADD TeamViewTemplate.json /templates/TeamViewTemplate.json
 ADD configure_notebooks.py /configure_notebooks.py
 ADD team-view-entrypoint.sh /team-view-entrypoint.sh
 
+# To try this out, outside of the kubernetes application environment, add an extract.json to the image
+#ADD sample.extract.json /root/extract/extract.json
+
 CMD ["/usr/local/bin/start-notebook.sh"]
 ENTRYPOINT ["/team-view-entrypoint.sh"]
--- configure_notebooks.py
+++ configure_notebooks.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import json
+import pathlib
+import shutil
 import jinja2
 from tv_extract.util.cli import extract_config_from_json
 
@@ -11,7 +13,18 @@ if __name__ == "__main__":
     template = templateEnv.get_template("TeamViewTemplate.json")
 
     with open('/root/extract/extract.json', 'r') as config_file:
-        config = extract_config_from_json(json.load(config_file))
+        json_config = json.load(config_file)
+        config = extract_config_from_json(json_config)
+        # first run jinja to create seperate notebooks for each project
         for extract in config.extracts:
             with open(f'/home/jovyan/project/{extract.name}.ipynb', 'w') as file:
                 file.write(template.render(extract=extract))
+        # Now, setup AWS region if present
+        if 'awsregion' in json_config:
+            aws_dir = pathlib.Path('/home/jovyan/.aws')
+            aws_dir.mkdir(parents=True, exist_ok=True)
+            aws_config_file = aws_dir / 'config'
+            with aws_config_file.open(mode='w') as aws_config:
+                aws_config.write(f"[profile ds-notebook]\nregion = {json_config['awsregion']}\n\n")
+                shutil.chown(str(aws_config_file.resolve()), user='jovyan', group='users')
+            shutil.chown(str(aws_dir.resolve()), user='jovyan', group='users')
